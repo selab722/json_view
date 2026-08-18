2d17143473a73a5d06502c09bf8e272a20654a27
Your Name <you@example.com>
2026-08-18 23:00:36 +0800

    remove compile.sh

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cmake -S . -B build
-
-cmake --build build --parallel --config Release
-
-# cmake --install build
--- src/ave/gui/window/opengl_window.cpp
+++ src/ave/gui/window/opengl_window.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <filesystem>
 #include <limits>
 #include <glad/gl.h>
 #include "imgui.h"
